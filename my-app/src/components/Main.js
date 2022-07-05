d0d04fe0b63ce6a679fb9645aba26080521979dc
Usman Fawad <ufawad0@gmail.com>
2022-07-05 15:21:48 +0500

    Project One done!

---
+++ my-app/src/components/Main.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function Main() {
+    return(
+        <main>
+            <h1 className="main--title">Why am I learning React?</h1>
+            <ul className="main--facts">
+                <li>It is Dynamic</li>
+                <li>It uses the component based concept</li>
+                <li>It has a huge community</li>
+            </ul>
+        </main>
+    )
+}
